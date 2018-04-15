203ad8da7b85fc083c6557bac51a4127bf958d4a
Tengis B <tengis617@users.noreply.github.com>
2018-04-15 14:27:37 +0700

    improved registration form

--- app/src/components/registration/BasicInformationStep.js
+++ app/src/components/registration/BasicInformationStep.js
@@ -1,41 +1,50 @@
 import React from 'react'
+import { Field, reduxForm } from 'redux-form'
 
-import { Header, Card, Button, Row, Col } from '../ui'
+import { Header, Card, Button, Row, Col, InputField } from '../ui'
 
-export default () => (
+const BasicInformationStep =  ({ handleSubmit, previousPage }) => (
   <div className="max-w-md container mx-auto">
+  <form onSubmit={handleSubmit}>
     <Card>
       <div>
         <Header size="4xl">Basic Information</Header>
       </div>
       <div className="max-w-sm mx-auto">
         <div>
-          <Label text="company name" forInput="companyName" />
-          <Input id="companyName" type="text" placeholder="company name" />
-        </div>
-        <div>
-          <Label text="address" forInput="address" />
-          <Input id="address" type="text" placeholder="address" />
-        </div>
-        <div>
-          <Label text="tags" forInput="tags" />
-          <Input id="tags" type="text" placeholder="tags" />
-        </div>
-        <div>
-          <Label text="short description" forInput="description" />
-          <Input id="description" type="text" placeholder="description" />
+        <Field
+              name="name"
+              label="company name"
+              component={InputField}
+              type="text"
+            />
+        <Field
+              name="description"
+              label="description"
+              component={InputField}
+              type="text"
+            />
         </div>
       </div>
+      <Row>
+            <Col size="1/2">
+              <div className="text-center p-6">
+                <Button text="Back" />
+              </div>
+            </Col>
+            <Col size="1/2">
+              <div className="text-center p-6">
+                <Button type="submit" text="Next" />
+              </div>
+            </Col>
+          </Row>
     </Card>
+    </form>
   </div>
 )
 
-const Input = ({ id, type, placeholder}) => (
-  <input className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4 mb-3" id={id} type={type} placeholder={placeholder} />
-)
-
-const Label = ({text, forInput }) => (
-  <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for={forInput}>
-    {text}
-  </label>
-)
+export default reduxForm({
+  form: 'companyRegistration',
+  destroyOnUnmount: false,
+  forceUnregisterOnUnmount: true,
+})(BasicInformationStep)
--- app/src/components/registration/CategorySelectionStep.js
+++ app/src/components/registration/CategorySelectionStep.js
@@ -1,54 +1,63 @@
 import React from 'react'
+import { Field, reduxForm } from 'redux-form'
 
 import { Header, Card, Button, Row, Col } from '../ui'
 
-export default () => (
-  <div className="max-w-md container mx-auto">
-    <Card>
-      <div>
-        <Header size="4xl">What kind of place are you listing?</Header>
-      </div>
-      <div className="max-w-sm mx-auto">
-        <Row>
-          <Box>
-            <i className="fas fa-utensils text-2xl" />
-            <div>Restaurant</div>
-          </Box>
-          <Box>
-            <i className="fas fa-gamepad text-2xl" />
-            <div>Entertainment</div>
-          </Box>
-        </Row>
-        <Row>
-          <Box>
-            <i className="fas fa-coffee text-2xl" />
-            <div>Coffee Shop</div>
-          </Box>
-          <Box>
-            <i className="fas fa-truck text-2xl" />
-            <div>Delivery Service</div>
-          </Box>
-        </Row>
-        <Row>
-          <Col size="1/2">
-          <div className="text-center p-6">
-            <Button text="Back" />
-          </div>
-          </Col>
-          <Col size="1/2">
-          <div className="text-center p-6">
-            <Button text="Next" />
-          </div>
-          </Col>
-        </Row>
-      </div>
-    </Card>
-  </div>
+const CategorySelectionStep = ({ handleSubmit, previousPage }) => (
+  <form onSubmit={handleSubmit}>
+    <div className="max-w-md container mx-auto">
+      <Card>
+        <div>
+          <Header size="4xl">What kind of place are you listing?</Header>
+        </div>
+        <div className="max-w-sm mx-auto">
+          <Row>
+            <Box>
+              <i className="fas fa-utensils text-2xl" />
+              <div>Restaurant</div>
+            </Box>
+            <Box>
+              <i className="fas fa-gamepad text-2xl" />
+              <div>Entertainment</div>
+            </Box>
+          </Row>
+          <Row>
+            <Box>
+              <i className="fas fa-coffee text-2xl" />
+              <div>Coffee Shop</div>
+            </Box>
+            <Box>
+              <i className="fas fa-truck text-2xl" />
+              <div>Delivery Service</div>
+            </Box>
+          </Row>
+          <Row>
+            <Col size="1/2">
+              <div className="text-center p-6">
+                <Button type="button" handleClick={previousPage} text="Back" />
+              </div>
+            </Col>
+            <Col size="1/2">
+              <div className="text-center p-6">
+                <Button type="submit" text="Next" />
+              </div>
+            </Col>
+          </Row>
+        </div>
+      </Card>
+    </div>
+  </form>
 )
 
 
-const Box = ({ children }) => (
-  <div className="cursor-pointer hover:bg-purple hover:text-white w-1/2 p-6 items-stretch text-center flex-1 content-center">
+const Box = ({ children, onClick, selected }) => (
+  <div onClick={onClick} className={`cursor-pointer hover:bg-purple ${selected ? "bg-purple-light text-white" : "hover:text-white"}  w-1/2 p-6 items-stretch text-center flex-1 content-center`}>
     {children}
   </div>
 )
+
+export default reduxForm({
+  form: 'companyRegistration',
+  destroyOnUnmount: false,
+  forceUnregisterOnUnmount: true,
+})(CategorySelectionStep)
